94cfe58c50a51e6979bb158b882909f0b8e02480
Istiniel <eclipsio@mail.ru>
2023-06-17 18:31:05 +0300

    feat: compress task done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import getDirname from '../getDirname.js';
+import { pipeline } from 'stream';
+const __dirname = getDirname(import.meta.url);
+
 const compress = async () => {
-    // Write your code here 
+  const fileToRead = path.resolve(__dirname, 'files/fileToCompress.txt');
+  const fileToWrite = path.resolve(__dirname, 'files/archive.gz');
+
+  const gZip = createGzip();
+  const readStream = createReadStream(fileToRead);
+  const writeStream = createWriteStream(fileToWrite);
+  pipeline(readStream, gZip, writeStream, (err) => console.log(err));
 };
 
 await compress();
