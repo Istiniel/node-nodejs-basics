0041dc943378704335fe38db50681cd63bf907c8
Istiniel <eclipsio@mail.ru>
2023-06-17 17:48:01 +0300

    feat: write stream task done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,14 +1,15 @@
-import { createReadStream } from 'node:fs';
+import { createWriteStream } from 'node:fs';
 import path from 'path';
 import getDirname from '../getDirname.js';
+import { stdin } from 'node:process';
 
 const __dirname = getDirname(import.meta.url);
 
 const write = async () => {
-  const filePath = path.resolve(__dirname, 'files/fileToRead.txt');
+  const filePath = path.resolve(__dirname, 'files/fileToWrite.txt');
 
-  const readStream = createReadStream(filePath, 'utf-8');
-  readStream.on('data', (data) => console.log(data));
+  const writeStream = createWriteStream(filePath, 'utf-8');
+  stdin.on('data', (data) => writeStream.write(data));
 };
 
 await write();
