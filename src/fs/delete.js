2564029f8c74ab1c17f4209775d54b7ef10ae3ef
Istiniel <eclipsio@mail.ru>
2023-06-17 15:10:17 +0300

    feat: delete task done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { rm, stat } from 'node:fs/promises';
+import path from 'path';
+import getDirname from '../getDirname.js';
+
+const __dirname = getDirname(import.meta.url);
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    await stat(filePath);
+    await rm(filePath, { recursive: false });
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw error;
+    }
+  }
 };
 
 await remove();
