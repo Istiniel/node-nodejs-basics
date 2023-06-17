0c177ad2738138e6a2fe364cf0d588ce55c4ccbf
Istiniel <eclipsio@mail.ru>
2023-06-17 14:11:43 +0300

    creat.js task done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { writeFile, stat } from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, 'files', 'fresh.txt');
+
+  try {
+    const stats = await stat(filePath);
+    if (stats) {
+      throw new Error('FS operation failed');
+    }
+  } catch (error) {
+    if (error.message === 'FS operation failed') {
+      throw error;
+    }
+    const promise = await writeFile(filePath, 'I am fresh and young');
+  }
 };
 
 await create();
