000d7368727cf0fafb96930d6748b58c7abdaf96
Istiniel <eclipsio@mail.ru>
2023-06-17 17:29:20 +0300

    feat: read stream task done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from 'node:fs';
+import path from 'path';
+import getDirname from '../getDirname.js';
+
+
+const __dirname = getDirname(import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, 'files/fileToRead.txt');
+
+  const readStream = createReadStream(filePath, 'utf-8');
+  readStream.on('data', (data) => console.log(data));
 };
 
 await read();
